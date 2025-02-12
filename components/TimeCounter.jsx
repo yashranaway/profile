b45bfa2abb8e53eaf8a03ac3069a1450906096f6
Aditya Garud <153842990+yashranaway@users.noreply.github.com>
2025-02-12 10:46:48 +0530

    Update TimeCounter.jsx
    time

--- components/TimeCounter.jsx
+++ components/TimeCounter.jsx
@@ -10,15 +10,13 @@ const TimeCounter = ({ startDate }) => {
       const now = new Date()
       const difference = now.getTime() - startDate.getTime()
 
-      const years = Math.floor(difference / (1000 * 60 * 60 * 24 * 365))
-      const months = Math.floor((difference % (1000 * 60 * 60 * 24 * 365)) / (1000 * 60 * 60 * 24 * 30))
-      const days = Math.floor((difference % (1000 * 60 * 60 * 24 * 30)) / (1000 * 60 * 60 * 24))
+      const days = Math.floor(difference / (1000 * 60 * 60 * 24))
       const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
       const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60))
       const seconds = Math.floor((difference % (1000 * 60)) / 1000)
 
       setTimeElapsed(
-        `${years} years, ${months} months, ${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`,
+        `${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`,
       )
     }
 
@@ -36,4 +34,3 @@ const TimeCounter = ({ startDate }) => {
 }
 
 export default TimeCounter
-
